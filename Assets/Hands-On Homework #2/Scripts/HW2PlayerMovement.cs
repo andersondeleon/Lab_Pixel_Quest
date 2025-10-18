499a210cfa7a7545487eea5e11b035856ccda7f2
andersondeleon <andersondeleon1204@gmail.com>
2025-10-18 16:02:33 -0400

    a

--- Assets/Hands-On Homework #2/Scripts/HW2PlayerMovement.cs
+++ Assets/Hands-On Homework #2/Scripts/HW2PlayerMovement.cs
@@ -1,6 +1,28 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class HW2PlayerMovement : MonoBehaviour
 {
+    Rigidbody2D _rigidbody2D;
+
+    private float _xspeed;
+    private float _yspeed;
+
+    public float speed = 3;
+
+    private string InputX = "Horizontal";
+    private string InputY = "Vertical";
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        _xspeed = Input.GetAxis(InputX);
+        _yspeed = Input.GetAxis(InputY);
+
+        _rigidbody2D.velocity = new Vector2(_xspeed, _yspeed) * speed;
+    }
 
 }
