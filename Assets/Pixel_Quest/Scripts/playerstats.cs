b40374b0793ebf1ddc680f596d54f9d6aa439a8f
andersondeleon <andersondeleon1204@gmail.com>
2025-11-08 12:09:11 -0500

    f

--- Assets/Pixel_Quest/Scripts/playerstats.cs
+++ Assets/Pixel_Quest/Scripts/playerstats.cs
@@ -5,24 +5,50 @@ using UnityEngine.SceneManagement;
 
 public class playerstats : MonoBehaviour
 {
+    public Transform respawnPoint;
     public string nextlevel = "scene_2";
+    public int CoinCounter = 0;
+    private int _Health = 3;
+    private int _maxhealth = 3;
     private void OnTriggerEnter2D(Collider2D collision) { 
         Debug.Log("hit");
         switch (collision.tag)
         {
             case "Death":
                 {
-                    string thislevel = SceneManager.GetActiveScene().name;
-                    SceneManager.LoadScene(thislevel);
+                    _Health--;
+                    if (_Health <= 0) {
+                        string thislevel = SceneManager.GetActiveScene().name;
+                        SceneManager.LoadScene(thislevel);
+                    }
+                    else
+                    {
+                        transform.position = respawnPoint.position;
+                    }
+                        
+                    
                     break;
                 }
+                
             case "Finish":
                 {
+                    string nextlevel = collision.transform.GetComponent<levelgoal>().nextlevel;
                     SceneManager.LoadScene(nextlevel);
                     break;
                 }
 
+            case "Coin":
+                CoinCounter++;
+                Destroy(collision.gameObject);
+                break;
 
+            case "Health":
+                {
+                    Destroy(collision.gameObject);
+                    _Health++;
+                    break;
+
+                }
         }
-    }
+    } 
 }
